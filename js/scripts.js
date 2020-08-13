a7478f3d7e8f9fc0403596e55c87ebc19943d4eb
Garrett Brown <garrettpaulbrown@gmail.com>
2020-08-13 13:54:51 -0700

    Populate HTML.
    Co-authored-by: Brian Harvey <brian.harv3y@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+  if (a === b === c) {
+    $('#equilateral').show().
+  }
+
+});
+
+
+
+/*
+
+	•	Equilateral: All sides are equal.
+	•	Isosceles: Exactly 2 sides are equal.
+	•	Scalene: No sides are equal.
+	•	NOT a triangle: The sum of the lengths of any two sides of a triangle is less than or equal to the length of the third side.
+	•	
+
+E = (a === b === c)
+I = (a === b && c)
+S= (a || b || c)
+NT= (a+b <= c || b+c <= a || a+c <= b) 
+*/
+
